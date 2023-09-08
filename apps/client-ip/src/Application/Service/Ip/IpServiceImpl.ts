b62000f472dc18832aea73108bb5daf29b4edc75
microld <2627500295@qq.com>
2023-09-08 18:00:40 +0800

    feat: get client ip

--- apps/client-ip/src/Application/Service/Ip/IpServiceImpl.ts
+++ apps/client-ip/src/Application/Service/Ip/IpServiceImpl.ts
@@ -8,23 +8,17 @@ import { IpService } from './IpService';
 
 @Injectable()
 export class IpServiceImpl implements IpService {
+  // https://github.com/WolfieLeader/get-client-ip/blob/main/src/index.ts
   public getClientIp(request: FastifyRequest): string {
-    if (get(request.headers, 'x-forwarded-for')) {
-      return get(request.headers, 'x-forwarded-for');
-    }
-
-    if (get(request.headers, 'x-forwarded-for')) {
-      return get(request.headers, 'x-real-ip');
-    }
-
-    if (get(request.headers, 'proxy-client-ip')) {
-      return get(request.headers, 'proxy-client-ip');
-    }
-
-    if (get(request.headers, 'wl-proxy-client-ip')) {
-      return get(request.headers, 'wl-proxy-client-ip');
-    }
-
-    return request.ip;
+    return (
+      get(request.headers, "x-forwarded-for") ||
+      get(request.headers, "x-real-ip") ||
+      get(request.headers, "proxy-client-ip") ||
+      get(request.headers, "wl-proxy-client-ip") ||
+      get(request.headers, "fly-client-ip") ||
+      get(request.headers, "true-client-ip") ||
+      get(request.headers, "cf-connecting-ip") ||
+      request.ip
+    );
   }
 }
